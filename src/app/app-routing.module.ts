5f07d0b9ad3033fc020d4d8a0188eca05c1efc87
Morras <iniamorras@gmail.com>
2023-03-20 10:33:10 -0300

    ^x ^X ^/ x X :x! ^X

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path:"",component: AppComponent,pathMatch: "full"},
+  {path:"Login",component: LoginComponent,pathMatch: "full"},
+  {path:"Register",component: RegisterComponent,pathMatch: "full"},
+  {path:"Home",component: HomeComponent,pathMatch: "full"}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
+export const routing = RouterModule.forRoot(appRoutes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './componentes/barra-navegacion/barra-navegacion.component';
+import { HeroComponent } from './componentes/hero/hero.component';
+import { AboutMeComponent } from './componentes/about-me/about-me.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ConocimientosComponent } from './componentes/conocimientos/conocimientos.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PorfolioService } from './servicios/porfolio.service';
+import { HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component'; 
+import { CookieService } from 'ngx-cookie-service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    HeroComponent,
+    AboutMeComponent,
+    ExperienciaComponent,
+    ConocimientosComponent,
+    ProyectosComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
